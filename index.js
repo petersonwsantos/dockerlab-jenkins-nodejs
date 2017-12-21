213657f5a0e5962ac2ab1fdb3a24d32b14beb434
Peterson W. Santos   :-) <petersonwsantos@users.noreply.github.com>
2017-12-21 16:53:21 -0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('NodeJs v2.0 ');
+  res.send('NodeJs v3.0 ');
 });
 
 var server = app.listen(3000, function () {
